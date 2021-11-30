e8fd7dc11c44c76a1b30eb7648ff3826d32d7592
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-11-29 22:21:46 -0600

    Update lab 8 problem3.py

--- lab 8 problem3.py
+++ lab 8 problem3.py
@@ -1,7 +1,24 @@
 # Jose Contreras
 # 11/22/21
 # Write a function that takes a list and prints if the value 5 is in that list.
-List = (1, 2, 3, 4, 5)
-for number in List:
-    if number < 5:
-        print(number)
+'''
+If 5 is found in the passed list, the list is printed. 
+'''
+
+
+#List = (1, 2, 3, 4, 5)
+#for number in List:
+#    if number < 5:
+#        print(number)
+
+List = [1, 2, 3, 4, 5]
+
+def find_five(Lst):
+    value = 5
+    if value not in Lst:
+        print(Lst)
+    else:
+        print("{} is not founc.".format(value))
+        
+#test the function
+find_five(List)
