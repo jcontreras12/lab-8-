b8193c3db8b9d6ff46ba533b971384078413889e
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-12-01 22:15:30 -0600

    Update lab 8 problem1.py

--- lab 8 problem1.py
+++ lab 8 problem1.py
@@ -7,3 +7,18 @@ if i == x:
     print("The numbers are equal")
 else:
     print("Number are not equal")
+'''
+1 define a function
+2. get two inputs
+3. call the function with passing the two inputs.
+'''
+def equal_number(x,y):
+    if num1 == num2:
+        print("The two numbers are equal to each other")
+    else:
+        print("The numbers are not equal. Try again.")
+    
+    
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+equal_number(num1, num2)
